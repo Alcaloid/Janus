8a61386b751ce323d0b545ead339b9193e89665b
waramporn <581606372@go.buu.ac.th>
2019-09-05 10:12:53 +0700

    commit activity , res and layout file from previous commit and implement  androidExtensions experimental = true to use @Parcelize

---
+++ app/src/main/java/com/codemobile/hackcatonapp/LoanDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.codemobile.hackcatonapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoanDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loan_detail)
+    }
+}

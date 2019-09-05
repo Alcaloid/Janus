77f69632fd60e8189a57b2b9cd18b8c34cb21ad4
waramporn <581606372@go.buu.ac.th>
2019-09-05 11:53:53 +0700

    display loan data to Detail Activity and modified LoanList Activity to show data by res/string

--- app/src/main/java/com/codemobile/hackcatonapp/LoanDetailActivity.kt
+++ app/src/main/java/com/codemobile/hackcatonapp/LoanDetailActivity.kt
@@ -2,11 +2,29 @@ package com.codemobile.hackcatonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_loan_detail.*
+import java.text.DecimalFormat
 
 class LoanDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loan_detail)
+
+        val loan = intent.getParcelableExtra<Loan>("Loan") ?: return
+
+        val formatter = DecimalFormat("#,###,###.##")
+        val limit = formatter.format(loan.limit)
+        limitLoan.text = getString(R.string.loan_list_limit, limit)
+        interestLoan.text = getString(R.string.laon_list_interest, loan.interest)
+        periodLoan.text = getString(R.string.loan_list_period, loan.period)
+        lonerName.text = loan.loner
+
+        submitLoanBtn.setOnClickListener {
+            Log.d("test","submitLoanBtn click")
+        }
+
+
     }
 }
--- app/src/main/java/com/codemobile/hackcatonapp/LoanListActivity.kt
+++ app/src/main/java/com/codemobile/hackcatonapp/LoanListActivity.kt
@@ -3,13 +3,14 @@ package com.codemobile.hackcatonapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.codemobile.hackcatonapp.LoanListActivity.*
+import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.activity_loan_list.*
 import kotlinx.android.synthetic.main.loan_list_card.view.*
 import java.text.DecimalFormat
@@ -20,12 +21,11 @@ class LoanListActivity() : AppCompatActivity(), OnLoanClick {
     private var loanArray: ArrayList<Loan> = arrayListOf(
         Loan(100000.00, 3, 3, "Waramporn Yangkijkarn"),
         Loan(200000.00, 2, 4, "Mikasa Ackerman"),
-        Loan(6000000.00, 5, 5, "Tanjirou Kamado"),
+        Loan(6000000.93, 5, 5, "Tanjirou Kamado"),
         Loan(800000.00, 5, 5, "Nezuko Kamado"),
-        Loan(2000000.00, 5, 5, "Kotarou Kashima"),
+        Loan(2000000.60, 5, 5, "Kotarou Kashima"),
         Loan(100000.00, 5, 5, "Ryuichi Kashima"),
         Loan(900000.00, 5, 5, "Phichit Chulanon")
-
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,13 +45,14 @@ class LoanListActivity() : AppCompatActivity(), OnLoanClick {
 
     override fun onLoanClick(loan: Loan) {
         var intent = Intent(this, LoanDetailActivity::class.java)
+        intent.putExtra("Loan", loan)
         startActivity(intent)
     }
 
 
-    inner class LoanListAdapter(private val listener: OnLoanClick) : RecyclerView.Adapter<loanListHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): loanListHolder {
-            return loanListHolder(
+    inner class LoanListAdapter(private val listener: OnLoanClick) : RecyclerView.Adapter<LoanListHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LoanListHolder {
+            return LoanListHolder(
                 LayoutInflater.from(parent.context).inflate(
                     R.layout.loan_list_card,
                     parent,
@@ -64,16 +65,15 @@ class LoanListActivity() : AppCompatActivity(), OnLoanClick {
             return loanArray.size
         }
 
-        override fun onBindViewHolder(holder: loanListHolder, position: Int) {
+        override fun onBindViewHolder(holder: LoanListHolder, position: Int) {
             val item = loanArray[position]
 
-            val formatter = DecimalFormat("#,###,###")
+            val formatter = DecimalFormat("#,###,###.##")
             val limit = formatter.format(item.limit)
-            holder.limitLoan.text = "Limit:  ${limit}฿"
-            //String.format("%.2f",getString(R.string.laon_list_limit, item.limit))
-            holder.interestLoan.text = "Interest:  ${item.interest}%"
-            //getString(R.string.laon_list_interest,item.interest)
-            holder.periodLoan.text = getString(R.string.laon_list_period, item.period)
+            holder.limitLoan.text = getString(R.string.loan_list_limit, limit)
+            holder.interestLoan.text = getString(R.string.laon_list_interest, item.interest)
+            holder.periodLoan.text = getString(R.string.loan_list_period, item.period)
+            holder.lonerName.text = item.loner
             holder.itemView.setOnClickListener {
                 listener.onLoanClick(item)
             }
@@ -81,23 +81,23 @@ class LoanListActivity() : AppCompatActivity(), OnLoanClick {
 
     }
 
-    class loanListHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+    class LoanListHolder(view: View) : RecyclerView.ViewHolder(view) {
         var limitLoan: TextView = view.limitLoan
         var interestLoan: TextView = view.interestLoan
         var periodLoan: TextView = view.periodLoan
         var lonerName: TextView = view.lonerName
-
     }
 
-    data class Loan(
-        val limit: Double,
-        val interest: Int,
-        val period: Int,
-        val loner: String
-    )
 }
 
 interface OnLoanClick {
     fun onLoanClick(loan: Loan)
 }
+
+@Parcelize
+data class Loan(
+    val limit: Double,
+    val interest: Int,
+    val period: Int,
+    val loner: String
+) : Parcelable

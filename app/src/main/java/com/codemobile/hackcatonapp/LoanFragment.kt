98a61017befad366e39567454eb949aed3c25336
waramporn <581606372@go.buu.ac.th>
2019-09-05 10:07:54 +0700

    create LoanList Activity and display data on list than link each loan list to DetailLoan Activity

--- app/src/main/java/com/codemobile/hackcatonapp/LoanFragment.kt
+++ app/src/main/java/com/codemobile/hackcatonapp/LoanFragment.kt
@@ -1,14 +1,23 @@
 package com.codemobile.hackcatonapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_loan.*
 
 class LoanFragment:Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_loan, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toLoanListBtn.setOnClickListener {
+            var intent = Intent(context, LoanListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

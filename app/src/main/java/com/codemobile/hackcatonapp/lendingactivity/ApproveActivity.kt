0daa5689cdf9ee039918c77a332b114ca4edbff1
Alcaloid <apichai.1507@mail.kmutt.ac.th>
2019-09-07 11:43:41 +0700

    Add card need approve

--- app/src/main/java/com/codemobile/hackcatonapp/lendingactivity/ApproveActivity.kt
+++ app/src/main/java/com/codemobile/hackcatonapp/lendingactivity/ApproveActivity.kt
@@ -3,6 +3,7 @@ package com.codemobile.hackcatonapp.lendingactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codemobile.hackcatonapp.R
+import com.codemobile.hackcatonapp.USER_LIST
 import kotlinx.android.synthetic.main.activity_approve.*
 
 class ApproveActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class ApproveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_approve)
+
+        var userArray:ArrayList<String> = intent.getStringArrayListExtra(USER_LIST) as ArrayList<String>
 
         btn_backFromApprove.setOnClickListener {
             finish()

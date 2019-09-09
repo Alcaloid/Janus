7aad9f6aacde85452a742edf570f47d11c9512f4
Monthira Thimachai <monthira.thimachai@scb.co.th>
2019-09-09 21:24:58 +0700

    -change image home

--- app/src/main/java/com/codemobile/hackcatonapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/codemobile/hackcatonapp/fragment/HomeFragment.kt
@@ -6,11 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.codemobile.hackcatonapp.R
+import com.codemobile.hackcatonapp.USER_ID_LENDER
+import com.codemobile.hackcatonapp.USER_ID_LOANER
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val _view = inflater.inflate(R.layout.fragment_home, container, false)
+
+            if (USER_ID_LENDER == "1" && USER_ID_LOANER == "1") {
+                _view.profileimg.setImageResource(R.drawable.profile01)
+            }else if (USER_ID_LENDER == "2" && USER_ID_LOANER == "2"){
+                _view.profileimg.setImageResource(R.drawable.profile02)
+            }
+        return _view
     }
 }

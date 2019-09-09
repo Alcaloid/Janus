e0e9b25069f7e939213462fe89867d957d731ace
waramporn <581606372@go.buu.ac.th>
2019-09-09 15:29:15 +0700

    create deep link

--- app/src/main/java/com/codemobile/hackcatonapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/codemobile/hackcatonapp/fragment/HomeFragment.kt
@@ -5,12 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.codemobile.hackcatonapp.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import android.content.Intent
+import android.net.Uri
+
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(com.codemobile.hackcatonapp.R.layout.fragment_home, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        deepLink.setOnClickListener {
+            val uri = Uri.parse("scbeasysim://billpayment-anonymous/6d53a6c7-d31b-4f11-84ee-7f5ffdd9ebf1")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+    }
+
 }
